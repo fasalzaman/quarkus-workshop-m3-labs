7f09fc602a795e4527fb9d069e58e5533e5fcdf0
kondetivenki <69524024+kondetivenki@users.noreply.github.com>
2021-05-03 18:00:20 +0530

    Update deploy-visits-service.sh

--- scripts/deploy-visits-service.sh
+++ scripts/deploy-visits-service.sh
@@ -4,10 +4,6 @@ echo Deploy visits-service........
 
 oc delete all -l app=visits-database
 
-oc new-app -e POSTGRESQL_USER=demouser@coolstore1 \
-  -e POSTGRESQL_PASSWORD=demo@pass123 \
-  -e POSTGRESQL_DATABASE=visits openshift/postgresql:latest \
-  --name=visits-database
 
 mvn clean package -DskipTests -Duser.home=/home/jboss -f $CHE_PROJECTS_ROOT/quarkus-workshop-m3-labs/quarkus-petclinic-visits-service
 
