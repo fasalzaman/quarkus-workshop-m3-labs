87837822ff183ca63023ab2cbbc19bacc04c736f
kondetivenki <69524024+kondetivenki@users.noreply.github.com>
2021-05-03 15:09:40 +0530

    Update deploy-vets-service.sh

--- scripts/deploy-vets-service.sh
+++ scripts/deploy-vets-service.sh
@@ -6,11 +6,11 @@ oc delete all -l app=vets-database
 
 oc new-app openshift/postgresql:latest \
             --name=vets-database \
-            -e POSTGRESQL_USER=vets \
-            -e POSTGRESQL_PASSWORD=mysecretpassword \
+            -e POSTGRESQL_USER=demouser@coolstore1 \
+            -e POSTGRESQL_PASSWORD=demo@pass123 \
             -e POSTGRESQL_DATABASE=vets 
 
-mvn clean package -DskipTests -f $CHE_PROJECTS_ROOT/quarkus-workshop-m3-labs/quarkus-petclinic-vets-service
+mvn clean package -DskipTests -Duser.home=/home/jboss -f $CHE_PROJECTS_ROOT/quarkus-workshop-m3-labs/quarkus-petclinic-vets-service
 
 oc label deployment/vets-database app.openshift.io/runtime=postgresql --overwrite && \
 oc label deployment/vets-database app.kubernetes.io/part-of=vets-app --overwrite
