8e7d2500581ac22f5fd25b9975c7fe7498b1cd61
kondetivenki <69524024+kondetivenki@users.noreply.github.com>
2021-05-03 17:58:37 +0530

    Update deploy-customers-service.sh

--- scripts/deploy-customers-service.sh
+++ scripts/deploy-customers-service.sh
@@ -4,10 +4,6 @@ echo Deploy customers-service........
 
 oc delete all -l app=customers-database
 
-oc new-app -e POSTGRESQL_USER=demouser@coolstore1 \
-  -e POSTGRESQL_PASSWORD=demo@pass123 \
-  -e POSTGRESQL_DATABASE=customers openshift/postgresql:latest \
-  --name=customers-database
 
 mvn clean package -DskipTests -Duser.home=/home/jboss -f $CHE_PROJECTS_ROOT/quarkus-workshop-m3-labs/quarkus-petclinic-customers-service
 
