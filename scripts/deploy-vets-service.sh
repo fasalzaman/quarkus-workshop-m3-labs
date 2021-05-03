4bc5808b7e9832f3945e8524a05c39d55c362c58
kondetivenki <69524024+kondetivenki@users.noreply.github.com>
2021-05-03 17:59:25 +0530

    Update deploy-vets-service.sh

--- scripts/deploy-vets-service.sh
+++ scripts/deploy-vets-service.sh
@@ -4,11 +4,6 @@ echo Deploy vets-service........
 
 oc delete all -l app=vets-database
 
-oc new-app openshift/postgresql:latest \
-            --name=vets-database \
-            -e POSTGRESQL_USER=demouser@coolstore1 \
-            -e POSTGRESQL_PASSWORD=demo@pass123 \
-            -e POSTGRESQL_DATABASE=vets 
 
 mvn clean package -DskipTests -Duser.home=/home/jboss -f $CHE_PROJECTS_ROOT/quarkus-workshop-m3-labs/quarkus-petclinic-vets-service
 
