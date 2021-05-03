5334533e4a1afa76bbbbff2eb43ef02402557460
kondetivenki <69524024+kondetivenki@users.noreply.github.com>
2021-05-03 15:06:03 +0530

    Update deploy-customers-service.sh

--- scripts/deploy-customers-service.sh
+++ scripts/deploy-customers-service.sh
@@ -4,12 +4,12 @@ echo Deploy customers-service........
 
 oc delete all -l app=customers-database
 
-oc new-app -e POSTGRESQL_USER=customers \
-  -e POSTGRESQL_PASSWORD=mysecretpassword \
+oc new-app -e POSTGRESQL_USER=demouser@coolstore1 \
+  -e POSTGRESQL_PASSWORD=demo@pass123 \
   -e POSTGRESQL_DATABASE=customers openshift/postgresql:latest \
   --name=customers-database
 
-mvn clean package -DskipTests -f $CHE_PROJECTS_ROOT/quarkus-workshop-m3-labs/quarkus-petclinic-customers-service
+mvn clean package -DskipTests -Duser.home=/home/jboss -f $CHE_PROJECTS_ROOT/quarkus-workshop-m3-labs/quarkus-petclinic-customers-service
 
 oc label deployment/customers-database app.openshift.io/runtime=postgresql --overwrite && \
 oc label deployment/customers-database app.kubernetes.io/part-of=customers-app --overwrite
