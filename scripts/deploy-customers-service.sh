dd3d81c1ba65e1a03b9f6399d5c97d2380a1f851
kondetivenki <69524024+kondetivenki@users.noreply.github.com>
2021-05-03 18:02:01 +0530

    Update deploy-customers-service.sh

--- scripts/deploy-customers-service.sh
+++ scripts/deploy-customers-service.sh
@@ -7,5 +7,5 @@ oc delete all -l app=customers-database
 
 mvn clean package -DskipTests -Duser.home=/home/jboss -f $CHE_PROJECTS_ROOT/quarkus-workshop-m3-labs/quarkus-petclinic-customers-service
 
-oc label deployment/customers-database app.openshift.io/runtime=postgresql --overwrite && \
-oc label deployment/customers-database app.kubernetes.io/part-of=customers-app --overwrite
+#oc label deployment/customers-database app.openshift.io/runtime=postgresql --overwrite && \
+#oc label deployment/customers-database app.kubernetes.io/part-of=customers-app --overwrite
