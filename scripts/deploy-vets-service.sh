bfa1f96c0f2089387720af119c42ed7af99d84d3
kondetivenki <69524024+kondetivenki@users.noreply.github.com>
2021-05-03 18:02:40 +0530

    Update deploy-vets-service.sh

--- scripts/deploy-vets-service.sh
+++ scripts/deploy-vets-service.sh
@@ -7,5 +7,5 @@ oc delete all -l app=vets-database
 
 mvn clean package -DskipTests -Duser.home=/home/jboss -f $CHE_PROJECTS_ROOT/quarkus-workshop-m3-labs/quarkus-petclinic-vets-service
 
-oc label deployment/vets-database app.openshift.io/runtime=postgresql --overwrite && \
-oc label deployment/vets-database app.kubernetes.io/part-of=vets-app --overwrite
+#oc label deployment/vets-database app.openshift.io/runtime=postgresql --overwrite && \
+#oc label deployment/vets-database app.kubernetes.io/part-of=vets-app --overwrite
