343d2dc88260f477171f88c9d2427ef7b07922cc
kondetivenki <69524024+kondetivenki@users.noreply.github.com>
2021-05-03 18:03:00 +0530

    Update deploy-visits-service.sh

--- scripts/deploy-visits-service.sh
+++ scripts/deploy-visits-service.sh
@@ -7,5 +7,5 @@ oc delete all -l app=visits-database
 
 mvn clean package -DskipTests -Duser.home=/home/jboss -f $CHE_PROJECTS_ROOT/quarkus-workshop-m3-labs/quarkus-petclinic-visits-service
 
-oc label deployment/visits-database app.openshift.io/runtime=postgresql --overwrite && \
-oc label deployment/visits-database app.kubernetes.io/part-of=visits-app --overwrite
+#oc label deployment/visits-database app.openshift.io/runtime=postgresql --overwrite && \
+#oc label deployment/visits-database app.kubernetes.io/part-of=visits-app --overwrite
